61e5871b9f18dd05b3adb0d0c9c1c77834147ed9
Aman Kumar Bairagi <amanbairagi1089@gmail.com>
2024-03-20 21:28:16 +0530

    Fixed the attributeValue populate bug and some bug in product page form with respect to saving the existing product which has been resolved

--- src/store/attributevalue/attributevalueAtom.jsx
+++ src/store/attributevalue/attributevalueAtom.jsx
@@ -7,7 +7,7 @@ export const attributeValueDataState = atom({
     default : selector({
         key :  "attributeValueSelector",
         get : async () => {
-            const res = await axios.get(`${import.meta.env.VITE_SERVER}/api/attributeValue/get-all-attributeValue/withoutpopulate`);
+            const res = await axios.get(`${import.meta.env.VITE_SERVER}/api/attributeValue/get-all-attributeValue`);
             return res.data.attributeValues
         }
     })
