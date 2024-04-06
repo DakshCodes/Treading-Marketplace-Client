4b0a644e02e2ae4e82fc9da419f03ad6481b475e
Aman Kumar Bairagi <amanbairagi1089@gmail.com>
2024-04-06 14:57:43 +0530

    Quick Challan Setup

--- src/components/SideNav/Sidenav.jsx
+++ src/components/SideNav/Sidenav.jsx
@@ -84,6 +84,14 @@ const Sidenav = () => {
                                 <svg xmlns="http://www.w3.org/2000/svg" width="35" height="30" viewBox="0 0 24 24" fill="none" stroke="#fff" strokeWidth="2" strokeLinecap="butt" strokeLinejoin="bevel"><path d="M14 2H6a2 2 0 0 0-2 2v16c0 1.1.9 2 2 2h12a2 2 0 0 0 2-2V8l-6-6z" /><path d="M14 3v5h5M16 13H8M16 17H8M10 9H8" /></svg>
                             )}
                         />
+                        <MenuItem
+                            href="/quick-challan"
+                            title="Quick Challan"
+                            classNames={`${path == '/quick-challan' && 'active-tab'}`}
+                            icon={(
+                                <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M12 2.75C6.89137 2.75 2.75 6.89137 2.75 12C2.75 17.1086 6.89137 21.25 12 21.25C17.1086 21.25 21.25 17.1086 21.25 12C21.25 6.89137 17.1086 2.75 12 2.75ZM1.25 12C1.25 6.06294 6.06294 1.25 12 1.25C17.9371 1.25 22.75 6.06294 22.75 12C22.75 17.9371 17.9371 22.75 12 22.75C6.06294 22.75 1.25 17.9371 1.25 12Z" fill="#1C274C"></path> <path fill-rule="evenodd" clip-rule="evenodd" d="M11.305 6.49549C11.5836 6.80199 11.561 7.27632 11.2545 7.55495L9.93995 8.75H16C16.4142 8.75 16.75 9.08579 16.75 9.5C16.75 9.91421 16.4142 10.25 16 10.25H9.93995L11.2545 11.445C11.561 11.7237 11.5836 12.198 11.305 12.5045C11.0263 12.811 10.552 12.8336 10.2455 12.555L7.4955 10.055C7.33914 9.91281 7.25 9.71131 7.25 9.5C7.25 9.28869 7.33914 9.08719 7.4955 8.94504L10.2455 6.44504C10.552 6.16641 11.0263 6.189 11.305 6.49549Z" fill="#1C274C"></path> <path fill-rule="evenodd" clip-rule="evenodd" d="M12.695 11.4955C12.4164 11.802 12.439 12.2763 12.7455 12.555L14.06 13.75H8C7.58579 13.75 7.25 14.0858 7.25 14.5C7.25 14.9142 7.58579 15.25 8 15.25H14.06L12.7455 16.445C12.439 16.7237 12.4164 17.198 12.695 17.5045C12.9737 17.811 13.448 17.8336 13.7545 17.555L16.5045 15.055C16.6609 14.9128 16.75 14.7113 16.75 14.5C16.75 14.2887 16.6609 14.0872 16.5045 13.945L13.7545 11.445C13.448 11.1664 12.9737 11.189 12.695 11.4955Z" fill="#1C274C"></path> </g></svg>
+                            )}
+                        />
                          
                          <MenuItem
                             href="/customers"
